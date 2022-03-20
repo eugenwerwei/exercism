a7b9e730a79167be69ee675cf6f9d1affaef1dfb
Eugen Werwei <eugen.werwei@outlook.com>
2022-03-20 21:15:56 +0100

    solved vehicle purchase exercise

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -1,16 +1,40 @@
 package purchase
 
+import "fmt"
+
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	if kind == "car" || kind == "truck" {
+		return true
+	}
+
+	return false
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in dictionary order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	var choice string
+
+	if option1 < option2 {
+		choice = option1
+	} else {
+		choice = option2
+	}
+
+	return fmt.Sprintf("%s is clearly the better choice.", choice)
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	var resellValue float64
+
+	if age <= 3 {
+		resellValue = .80
+	} else if age > 3 && age < 10 {
+		resellValue = .70
+	} else {
+		resellValue = .50
+	}
+
+	return originalPrice * resellValue
 }
