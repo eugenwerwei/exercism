ac192a1d98b9cfb10234ef2b86a8c2ce33459fc1
Eugen Werwei <werwei.developer@gmail.com>
2022-04-14 16:58:34 +0200

    solved logs logs logs exercise

--- go/logs-logs-logs/logs_logs_logs.go
+++ go/logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,42 @@
 package logs
 
+import "unicode/utf8"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, r := range log {
+		if r == '❗' {
+			return "recommendation"
+		} else if r == '🔍' {
+			return "search"
+		} else if r == '☀' {
+			return "weather"
+		}
+	}
+
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	var str string
+
+	for _, r := range log {
+		if r == oldRune {
+			str += string(newRune)
+		} else {
+			str += string(r)
+		}
+	}
+
+	return str
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	numOfRunes := utf8.RuneCountInString(log)
+
+	return numOfRunes <= limit
 }
