3f82797d3ba77765bc2583daf3331233259c799c
Eugen Werwei <werwei.developer@gmail.com>
2022-04-14 18:09:47 +0200

    wip the farm exercise

--- go/the-farm/the_farm.go
+++ go/the-farm/the_farm.go
@@ -1,10 +1,32 @@
 package thefarm
 
+import "errors"
+
 // See types.go for the types defined for this exercise.
 
 // TODO: Define the SillyNephewError type here.
+type SillyNephewError interface {
+	// wip
+}
 
 // DivideFood computes the fodder amount per cow for the given cows.
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
-	panic("Please implement DivideFood")
+	amount, err := weightFodder.FodderAmount()
+
+	if err != nil {
+		if err == ErrScaleMalfunction {
+			if amount < 0 {
+				return 0, errors.New("negative fodder")
+			}
+			amount *= 2
+		} else {
+			return 0, err
+		}
+	} else if amount < 0 {
+		return 0, errors.New("negative fodder")
+	} else if cows == 0 {
+		return 0, errors.New("division by zero")
+	}
+
+	return amount / float64(cows), nil
 }
