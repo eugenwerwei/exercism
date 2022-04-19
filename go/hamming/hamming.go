934a040d6ec020e705b5c10d77d3b3fdef1072a1
Eugen Werwei <werwei.developer@gmail.com>
2022-04-19 19:21:22 +0200

    solved hamming exercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,29 @@
+/*
+	Package hamming provides the function Distance(),
+	for counting the differences of two DNA sequences.
+
+	Example:
+		Distance("GAGCCTACTAACGGGAT", "CGTCGTAATGACGGCCG")
+		-> 9, nil
+*/
+
 package hamming
 
+import "errors"
+
+// Distance returns the counted differences of two strings as int.
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("Sequences are not of same length.")
+	}
+
+	count := 0
+
+	for i, char := range a {
+		if char != rune(b[i]) {
+			count++
+		}
+	}
+
+	return count, nil
 }
