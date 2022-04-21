3730fc58c190a05238552111e40f76db6aed7418
Eugen Werwei <werwei.developer@gmail.com>
2022-04-21 22:23:26 +0200

    solved isogram exercise

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,27 @@
 package isogram
 
+import (
+	"sort"
+	"strings"
+)
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	if word == "" {
+		return true
+	}
+
+	replacer := strings.NewReplacer("-", "", " ", "")
+	str := replacer.Replace(word)
+	str = strings.ToLower(str)
+
+	strArr := strings.Split(str, "")
+	sort.Strings(strArr)
+
+	for i := 0; i < len(strArr)-1; i++ {
+		if strArr[i] == strArr[i+1] {
+			return false
+		}
+	}
+
+	return true
 }
