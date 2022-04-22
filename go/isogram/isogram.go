d5bb7660de991ba26b426f2713710c2fcd97008b
Eugen Werwei <werwei.developer@gmail.com>
2022-04-22 12:53:28 +0200

    updated to faster solution, added comments

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,25 +1,36 @@
+/*
+	Package isogram provides the function IsIsogram(),
+	to determine if a string is an isogram.
+
+	Example:
+		IsIsogram("background")
+		-> true
+*/
 package isogram
 
-import (
-	"sort"
-	"strings"
-)
+import "strings"
 
+// IsIsogram returns true if a character in a word is unique.
 func IsIsogram(word string) bool {
 	if word == "" {
 		return true
 	}
 
-	replacer := strings.NewReplacer("-", "", " ", "")
-	str := replacer.Replace(word)
-	str = strings.ToLower(str)
+	str := strings.ToLower(word)
 
-	strArr := strings.Split(str, "")
-	sort.Strings(strArr)
+	for i := 0; i < len(str)-1; i++ {
+		if string(str[i]) == " " || string(str[i]) == "-" {
+			continue
+		}
 
-	for i := 0; i < len(strArr)-1; i++ {
-		if strArr[i] == strArr[i+1] {
-			return false
+		for j := i + 1; j < len(str); j++ {
+			if string(str[j]) == " " || string(str[j]) == "-" {
+				continue
+			}
+
+			if string(str[i]) == string(str[j]) {
+				return false
+			}
 		}
 	}
 
