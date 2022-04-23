432169e965460eb22433d5c8e46d68767da488e4
Eugen Werwei <werwei.developer@gmail.com>
2022-04-23 13:53:25 +0200

    solved luhn exercise

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,5 +1,49 @@
 package luhn
 
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	len := len(id)
+
+	id = strings.TrimSpace(id)
+
+	if len < 2 || id == "0" {
+		return false
+	}
+
+	sum := 0
+	isSecond := false
+
+	for i := len - 1; i >= 0; i-- {
+		if unicode.IsLetter(rune(id[i])) {
+			return false
+		}
+
+		if unicode.IsSpace(rune(id[i])) {
+			continue
+		}
+
+		digit, _ := strconv.Atoi(string(id[i]))
+
+		if isSecond {
+			digit = digit * 2
+
+			if digit > 9 {
+				sum += digit - 9
+			} else {
+				sum += digit
+			}
+		} else {
+
+			sum += digit
+		}
+
+		isSecond = !isSecond
+	}
+
+	return sum%10 == 0
 }
